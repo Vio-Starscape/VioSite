677fd7a697a2919285a0ada1da91e4d9d4a01998
Meaning <eric.minecraft20@gmail.com>
2024-05-20 01:47:21 -0400

    fixed: title

--- src/Loading.js
+++ src/Loading.js
@@ -3,8 +3,9 @@ import './main.css'; // Import the CSS file for styling
 
 const Loading = () => {
     return (
-        <div className="fixed inset-0 flex items-center justify-center">
+        <div className="fixed inset-0 flex flex-col items-center justify-center">
             <img className="logo animate-spin w-32 h-32" src="/android-chrome-512x512.png" alt="Loading logo" />
+            <h1 className="text-4xl">Loading...</h1>
         </div>
     );
 };
